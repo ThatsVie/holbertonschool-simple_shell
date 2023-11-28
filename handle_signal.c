da6b12361750228aab626c2f65579b5222568c3f
ThatsVie <Viepaulalmt@gmail.com>
2023-11-28 14:50:11 -0800

    signal handler

---
+++ handle_signal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * handle_signal - handles the SIGINT signal ( Ctrl + C)
+ * @signal: input signal number
+ * Description: 
+ * This function serves as a signal handler for the SIGINT signal.
+ * When the user presses Ctrl+C, this signal is sent to the process.
+ * The function catches this signal and prints a newline and the shell prompt.
+ */
+void handle_signal (int signal)
+{
+	if (signal == SIGINT)
+	{
+		puts("\n $ ");
+	}
+}
