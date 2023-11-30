d93cb1dd7f24d1abc5499aa38db3a9d859a0948c
ThatsVie <Viepaulalmt@gmail.com>
2023-11-30 06:34:50 -0800

    reworking command_executor.c

--- command_executor.c
+++ command_executor.c
@@ -7,58 +7,58 @@
  */
 int execute(char *user_command)
 {
-	char **command_arguments = NULL; /* array to store command arguments*/
-	char *command_path = NULL; /* path of the comand to be executed */
+	char **command_args = NULL; /* array to store command arguments */
+	char *command_path = NULL; /* path of command to be executed */
 	pid_t child_pid;
 	int child_status, exit_status = 0;
 
-	/* tokenize the user's command */
-	command_arguments = tokenize(user_command);
-	if (command_arguments == NULL)
+	/* tokenize user's command */
+	command_args = tokenize(user_input);
+	if (command_args == NULL)
 	{
-		fprintf(stderr, "Error: Failed to tokenize the command.\n");
-		return (-1);/*tokenization failed*/
+		return (-1); /* tokenization failed */
 	}
-	/* Is command an absolute path or needs path resolution */
-	if (user_command[0] == '/')
-		command_path = strdup(user_command);
-	else
-		command_path = get_full_path(command_arguments[0]);
 
+	/* check if command is absolute path of needs path resolution */
+	if (user_input[0] == '/')
+	{/* use input as path if it starts with '/' */
+		command_path = strdup(user_input);
+	}
+	else
+	{
+		command_path = get_full_path(command_args[0]);
+	}
 	/* check if get_full_path failed */
 	if (command_path == NULL)
 	{
-		fprintf(stderr, "Error: Command not found in PATH.\n");
-		free_tokens(command_arguments);
+		free_tokens(command_args);
 		return (-1);
 	}
 
-	/* Fork a child process for command execution */
-
+	/* fork a child process for command execution */
 	child_pid = fork();
 	if (child_pid < 0)
 	{
-		perror("Error: Failed to fork a child process.\n");
-		free_tokens(command_arguments);
+		free_tokens(command_args);
 		free(command_path);
-		return (-1);
+		return (-1); /* fork failed*/
 	}
-	else if (child_pid == 0)
+	else if (child_pid == 0) /*child process */
 	{
-		exit_status = execve(command_path, command_arguments, environ);
-	/* if execve fails print an error and exit child process */
+		exit_status = execve(command_path, command_args, environ);
+
+		/* if execve fails print error and exit child process*/
 		perror(command_path);
 		exit(exit_status);
 	}
-	else
+	else /* parent process */
 	{
 		wait(&child_status);
 		if (WIFEXITED(child_status))
 			exit_status = WEXITSTATUS(child_status);
 	}
-
 	/* free up and return exit status */
-	free_tokens(command_arguments);
+	free_token(command_args);
 	free(command_path);
 	return (exit_status);
 }
