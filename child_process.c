96c5de59bd736e7344758a3cf82c7abe7cfcb199
ThatsVie <Viepaulalmt@gmail.com>
2023-12-04 18:06:55 -0800

    commented child process

--- child_process.c
+++ child_process.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * child_process - creates child process and executes a command
- * @path: PATH environment variable
+ * @path: PATH environment variable (freed in function)
  * @c_path: full path of the command to execute
  * @c_args: arguments of the command
  * Return: Exit status of command
@@ -12,23 +12,28 @@ int child_process(char *path, char *c_path, char **c_args)
 	pid_t wait_stat;
 	int exit_stat = 0;
 
+	/* Free the dynamically allocated memory for the PATH variable */
 	free(path);
+	/* Create a child process using fork() */
 	child_pid = fork();
 
 	if (child_pid == 0)
 	{
-		/* execute command */
+		/* In the child process, execute the specified command */
 		if (execve(c_path, c_args, NULL) == -1)
-			exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE);/*Exit if execution fails */
 	}
 	else if (child_pid < 0)
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);/*Exit the parent process if fork fails */
 	else
 	{
+		/*In parent process, wait fo child process to complete*/
 		do {
 			wait_stat = waitpid(child_pid, &exit_stat, WUNTRACED);
 		} while (WIFEXITED(exit_stat) && !WIFSIGNALED(exit_stat));
 	}
+	/*Ignore the wait_stat variable to prevent compiler warnings*/
 	(void)wait_stat;
+	/*Return the exit status of the executed command */
 	return (exit_stat);
 }
